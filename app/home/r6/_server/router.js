2f67ca54a8447ecadc65dc388262e8797013cfa3
Fritz179 <Fritz179@users.noreply.github.com>
2020-05-11 10:24:42 +0200

    R6 audio IO, named connections

--- app/home/r6/_server/router.js
+++ app/home/r6/_server/router.js
@@ -12,7 +12,7 @@ module.exports = ({router, io}) => {
     if (!sockets.has(id)) {
       // forward offer
       socket.on('offer', (to, offer) => {
-        console.log(`offer from: ${id}, to: ${to}`);
+        // console.log(`offer from: ${id}, to: ${to}`);
         const toSocket = sockets.get(to)
         if (toSocket) {
           toSocket.emit('offer', id, offer)
@@ -29,25 +29,39 @@ module.exports = ({router, io}) => {
 
       // forward response
       socket.on('response', (to, response) => {
-        console.log(`aswer from: ${id}, to: ${to}`);
+        // console.log(`aswer from: ${id}, to: ${to}`);
         const toSocket = sockets.get(to)
         if (toSocket) {
           toSocket.emit('response', id, response)
         }
       })
 
+      // change name
+      socket.on('setName', (name) => {
+        console.log(`Renamed: ${socket.name}, to: ${name}`);
+        socket.name = name
+        io.of('r6').emit('setName', id, name)
+      })
+
       // remove on disconnect
       socket.on('disconnect', () => {
         io.of('r6').emit('disconnectPeer', id)
         sockets.delete(id)
+        console.log('Disconnected: ' + socket.name);
       })
 
       // start requests
       socket.emit('request-offer', [...sockets.keys()], id);
 
+      // default name to id
+      socket.name = socket.id
+      sockets.forEach((to, of) => {
+        socket.emit('setName', of, to.name)
+      })
+
       // add socket only ofter request has started
       sockets.set(id, socket)
-      console.log('Total r6 connected: ' + sockets.size);
+      console.log('New connection: ' + socket.name);
     }
   })
 }
--- app/home/r6/main.js
+++ app/home/r6/main.js
@@ -1,30 +1,112 @@
 (async () => {
   // create localStream
-  const localStream = await navigator.mediaDevices.getUserMedia({video: false, audio: true});
+  const localStream = new MediaStream()
+  const defaultAudio = await navigator.mediaDevices.getUserMedia({video: false, audio: true});
+  defaultAudio.getTracks().forEach(track => {
+    localStream.addTrack(track, localStream);
+  });
   // const localVideo = document.getElementById('local-video');
   // localVideo.srcObject = localStream;
   const container = document.getElementById('container')
+  const master = document.getElementById('master-volume')
+  master.oninput = () => {
+    const children = container.children
+    for (let i = 0; i < children.length; i++) {
+      const child = children[i]
+      const input = child.children[1]
+      const video = child.children[2]
+      video.volume = input.value * master.value
+    }
+  }
+
+  // select io
+  const audioInputSelect = document.getElementById('audioInput');
+  const audioOutputSelect = document.getElementById('audioOutput');
+  const selectors = [audioInputSelect, audioOutputSelect]
+  const devices = await navigator.mediaDevices.enumerateDevices()
+
+  const values = selectors.map(select => select.value);
+  selectors.forEach(select => select.innerHTML = '');
+
+  for (let i = 0; i !== devices.length; ++i) {
+    const deviceInfo = devices[i];
+    const option = document.createElement('option');
+    option.value = deviceInfo.deviceId;
+    if (deviceInfo.kind === 'audioinput') {
+      option.text = deviceInfo.label || `microphone ${audioInputSelect.length + 1}`;
+      audioInputSelect.appendChild(option);
+    } else if (deviceInfo.kind === 'audiooutput') {
+      option.text = deviceInfo.label || `speaker ${audioOutputSelect.length + 1}`;
+      audioOutputSelect.appendChild(option);
+    }
+  }
+
+  audioInputSelect.onchange = async () => {
+    localStream.getTracks().forEach(track => localStream.removeTrack(track))
+    const audioStream = await navigator.mediaDevices.getUserMedia({video: false, audio: {deviceId: audioInputSelect.value}});
+
+    audioStream.getTracks().forEach(track => {
+      localStream.addTrack(track, audioStream)
+    });
+  }
+
+  audioOutputSelect.onchange = async () => {
+    const outputID = audioOutputSelect.value
+
+    const children = container.children
+    for (let i = 0; i < children.length; i++) {
+      const video = children[i].children[2]
+      await video.setSinkId(outputID)
+    }
+  }
 
   // connect after streams are created
   const socket = connect('/r6')
   const connections = new Map()
+  const names = {}
   const rtcConfig = {iceServers: [{urls: ["stun:stun.l.google.com:19302"]}]}
 
   function addTrack(track, from) {
     // create stream
+    const child = document.createElement('div')
+    const label = document.createElement('label');
+    const input = document.createElement('input');
     const video = document.createElement('video');
+    child.id = from
+
+    label.for = `"${from}-volume`
+    label.innerHTML = `${names[from] || from}:`
+
+    input.min = 0
+    input.max = 1
+    input.max = 1
+    input.defaultValue = 1
+    input.step = 0.01
+    input.type = 'range'
+    input.id = label.for
+    input.oninput = () => video.volume = input.value * master.value
+
     const stream = new MediaStream();
-    container.appendChild(video)
     video.srcObject = stream;
     video.play()
-    video.id = from
 
-    stream.addTrack(event.track, stream)
+    child.appendChild(label)
+    child.appendChild(input)
+    child.appendChild(video)
+    container.appendChild(child)
+
+    stream.addTrack(track, stream)
     console.log('connected: ' + from);
   }
 
+
   socket.on('request-offer', (users, me) => {
-    console.log(me);
+    const name = document.getElementById('name')
+    name.oninput = () => {
+      const val = name.value
+      socket.emit('setName', val)
+    }
+
     container.innerHTML = ''
     users.forEach(async user => {
       // create conn
@@ -102,6 +184,17 @@
     if (el) el.remove()
     connections.delete(id)
   });
+
+  socket.on('setName', (of, to) => {
+    names[of] = to
+    const child = document.getElementById(of)
+    if (child) {
+      const label = child.children[0]
+      if (label) {
+        label.innerHTML = `${to}:`
+      }
+    }
+  })
 
   window.onunload = window.onbeforeunload = () => {
     socket.close();
