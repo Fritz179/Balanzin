781d4275250d3d0d445648a23df629150bfca328
Fritz179 <Fritz179@users.noreply.github.com>
2020-05-12 14:13:44 +0200

    Unbeatable tris
    Adjusted minimax function for tris
    Added colors for win/draw/loss
    Playable on phone

--- app/home/tris/main.js
+++ app/home/tris/main.js
@@ -1,11 +1,13 @@
 const board = []
+let done = false
 
 document.getElementById('button').onclick = () => {
+  elements.forEach(el => el.classList = [])
   board.splice(0, 9)
   display()
+  done = false
 }
 const elements = [...document.getElementsByClassName('gameboard')[0].children]
-let myTurn = true
 
 function display() {
   elements.forEach((el, i) => {
@@ -15,57 +17,72 @@ function display() {
 
 elements.forEach((el, i) => {
   el.addEventListener('click', () => {
-    if (myTurn && !board[i]) {
+    if (done) {
+      return
+    }
+
+    if (!board[i]) {
       board[i] = 1
-      doBestMove()
+
+      if (board[4]) {
+        const [_, pos] = miniMax(board, -1)
+        board[pos] = -1
+      } else {
+        board[4] = -1
+      }
+
+      const score = getScore(board)
+      if (score) {
+        done = true
+        if (score == 2) {
+          elements.forEach(el => el.classList.add('draw'))
+        } else {
+          getScore(board, true).forEach(pos => elements[pos].classList.add(score == 1 ? 'won' : 'lost'))
+        }
+      }
+
       display()
     }
   })
 })
 
-function doBestMove() {
-  const [score, i] = miniMax(board, -1)
-  board[i] = -1
-  console.log(i);
-}
-
 function miniMax(board, turn) {
-  let draw = -1
-  // console.log(board, turn);
+  let score = getScore(board)
+  if (score) {
+    return [score]
+  }
+
+  let move
+  let best = -turn
+
   for (let i = 0; i < 9; i++) {
     if (!board[i]) {
       const newBoard = [...board]
       newBoard[i] = turn
 
-      let score = getScore(newBoard)
-      if (score && score != -turn) {
-        return [score, i]
-      } else if (!score && draw == -1) {
-        draw = i
-      };
+      const [score] = miniMax(newBoard, -turn)
 
-      [score, newI] = miniMax(newBoard, -turn)
-      if (score == turn) {
-        return [score, i]
-      } else if (score == -turn) {
-        continue;
-      } else {
-        draw = i
+      if (best == -turn) {
+        best = score
+        move = i
+      } else if (score == turn) {
+        best = score
+        move = i
       }
     }
   }
 
-  return [0, draw]
+  return [best, move]
 }
 
-function getScore(board) {
+function getScore(board, pos = false) {
   for (let i = 0; i < 3; i++) {
-    if (board[i] && board[i] == board[i + 3] && board[i] == board[i + 6]) return board[i]
-    if (board[i * 3] && board[i * 3] == board[i * 3 + 1] && board[i * 3] == board[i * 3 + 2]) return board[i * 3]
+    if (board[i] && board[i] == board[i + 3] && board[i] == board[i + 6]) return pos ? [i, i + 3, i + 6] : board[i]
+    if (board[i * 3] && board[i * 3] == board[i * 3 + 1] && board[i * 3] == board[i * 3 + 2]) return pos ? [i * 3, i * 3 + 1, i * 3 + 2] : board[i * 3]
   }
 
-  if (board[0] && board[0] == board[4] && board[0] == board[8]) return board[4]
-  if (board[2] && board[2] == board[4] && board[2] == board[6]) return board[4]
+  if (board[0] && board[0] == board[4] && board[0] == board[8]) return pos ? [0, 4, 8] : board[4]
+  if (board[2] && board[2] == board[4] && board[2] == board[6]) return pos ? [2, 4, 6] : board[4]
 
   for (let i = 0; i < 9; i++) {
     if (!board[i]) return
