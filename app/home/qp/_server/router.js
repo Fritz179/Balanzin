854a0f8052a2987ebe7638cb2bca85e58cf51423
Fritz_179 <fabrizio.cortesi179@gmail.com>
2022-07-28 11:37:44 +0200

    QP: ML+LC+Optionals

--- app/home/qp/_server/router.js
+++ app/home/qp/_server/router.js
@@ -22,6 +22,16 @@ module.exports = ({router}) => {
     snapchat: 'xrampela',
   })
 
+  add('ml', {
+    title: 'Matteo Liver',
+    whatsapp: '41796361289'
+  })
+
+  add('lc', {
+    title: 'Luca Caspani',
+    whatsapp: '393474027933'
+  })
+
   router.use('/', (req, res) => {
     res.send('Salve! Quest link le niamo tacu a nient, scrivum sü whatsapp ca ta meti sü quel ca ta vos')
   })
